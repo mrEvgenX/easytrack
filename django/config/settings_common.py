578d70846bdf828894a1c5ed01b4b43b42a0ad7b
Evgene Petrenko <mrEvgenix@gmail.com>
2020-06-24 16:25:22 +0700

    Fix nginx settings and development ones

--- django/config/settings_common.py
+++ django/config/settings_common.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 EMAIL_HOST = os.getenv('DJANGO_EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('DJANGO_EMAIL_HOST_USER')
